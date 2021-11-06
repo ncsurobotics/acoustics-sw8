63ebad5667ca13c203658a0c21406a3bab471706
Bennett Petzold <bennettpetzold01@gmail.com>
2021-11-06 10:44:41 -0400

    Add user input
    Rewritten for loops.
    Added class and dictionary.
    Variable names changed to Python standard.

--- Python 2021/Pool_Test_code_2021_10_22.py
+++ Python 2021/Pool_Test_code_2021_10_22.py
@@ -12,272 +12,211 @@ import matplotlib.pyplot as plt
 from picosdk.functions import adc2mV, assert_pico_ok
 import csv
 
-# Create chandle and status ready for use
-chandle = ctypes.c_int16()
-status = {}
+# Channel letters to channel numbers
+numSwitch = {
+    "A":0,
+    "B":1,
+    "C":2,
+    "D":3
+}
 
-# Open 4000 series PicoScope
-# Returns handle to chandle for use in future API functions
-status["openunit"] = ps.ps4000aOpenUnit(ctypes.byref(chandle), None)
+class ChInfo:
 
-try:
-    assert_pico_ok(status["openunit"])
-except:
+    """Associated info for picoscope channels. """
 
-    powerStatus = status["openunit"]
+    def __init__(self, letter):
+        self.let = letter
+        self.num = numSwitch.get(letter)
+        self.b_max = None
+        self.adc2mVMax = None
 
-    if powerStatus == 286:
-        status["changePowerSource"] = ps.ps4000aChangePowerSource(chandle, powerStatus)
-    else:
-        raise
+while True: # Stops on no channel input
+    channels = []
+    print("Stops taking input on empty <CR>")
+    while True: # Stops on empty channel input
+        ch = input("Trigger channel: ")
+        if ch == "":
+            break
+        ch = ch.upper()
+        if ch not in channels:
+            channels.append(ChInfo(ch))
 
-    assert_pico_ok(status["changePowerSource"])
+    if not channels:
+        break # No channel inputs, end program
 
-# Set up channel A
-# handle = chandle
-# channel = PS4000a_CHANNEL_A = 0
-# enabled = 1
-# coupling type = PS4000a_DC = 1
-# range = PS4000a_2V = 7
-# analogOffset = 0 V
-chARange = 8
-status["setChA"] = ps.ps4000aSetChannel(chandle, 0, 1, 1, chARange, 0)
-assert_pico_ok(status["setChA"])
+    # Create chandle and status ready for use
+    chandle = ctypes.c_int16()
+    status = {}
 
-# Set up channel B
-# handle = chandle
-# channel = PS4000a_CHANNEL_B = 1
-# enabled = 1
-# coupling type = PS4000a_DC = 1
-# range = PS4000a_2V = 7
-# analogOffset = 0 V
-chBRange = 8
-status["setChB"] = ps.ps4000aSetChannel(chandle, 1, 1, 1, chBRange, 0)
-assert_pico_ok(status["setChB"])
+    # Open 4000 series PicoScope
+    # Returns handle to chandle for use in future API functions
+    status["openunit"] = ps.ps4000aOpenUnit(ctypes.byref(chandle), None)
 
-# Set up channel C
-# handle = chandle
-# channel = PS4000a_CHANNEL_C = 2
-# enabled = 1
-# coupling type = PS4000a_DC = 1
-# range = PS4000a_2V = 7
-# analogOffset = 0 V
-chCRange = 8
-status["setChC"] = ps.ps4000aSetChannel(chandle, 2, 1, 1, chCRange, 0)
-assert_pico_ok(status["setChC"])
+    try:
+        assert_pico_ok(status["openunit"])
+    except:
 
-# Set up channel D
-# handle = chandle
-# channel = PS4000a_CHANNEL_D = 3
-# enabled = 1
-# coupling type = PS4000a_DC = 1
-# range = PS4000a_2V = 7
-# analogOffset = 0 V
-chDRange = 8
-status["setChD"] = ps.ps4000aSetChannel(chandle, 3, 1, 1, chDRange, 0)
-assert_pico_ok(status["setChD"])
+        powerStatus = status["openunit"]
 
-# Set up channel E
-# handle = chandle
-# channel = PS4000a_CHANNEL_E = 1
-# enabled = 1
-# coupling type = PS4000a_DC = 1
-# range = PS4000a_2V = 7
-# analogOffset = 0 V
-chERange = 7
-status["setChE"] = ps.ps4000aSetChannel(chandle, 4, 0, 1, chERange, 0)
-assert_pico_ok(status["setChE"])
+        if powerStatus == 286:
+            status["changePowerSource"] = ps.ps4000aChangePowerSource(chandle, powerStatus)
+        else:
+            raise
 
-# Set up channel F
-# handle = chandle
-# channel = PS4000a_CHANNEL_F = 1
-# enabled = 1
-# coupling type = PS4000a_DC = 1
-# range = PS4000a_2V = 7
-# analogOffset = 0 V
-chFRange = 7
-status["setChF"] = ps.ps4000aSetChannel(chandle, 5, 0, 1, chFRange, 0)
-assert_pico_ok(status["setChF"])
+        assert_pico_ok(status["changePowerSource"])
 
-# Set up channel G
-# handle = chandle
-# channel = PS4000a_CHANNEL_G = 1
-# enabled = 1
-# coupling type = PS4000a_DC = 1
-# range = PS4000a_2V = 7
-# analogOffset = 0 V
-chGRange = 7
-status["setChG"] = ps.ps4000aSetChannel(chandle, 6, 0, 1, chGRange, 0)
-assert_pico_ok(status["setChG"])
+    # Characteristics shared by channels A-D
+    ENABLED = 1
+    COUPLING_TYPE = 1 # PS4000a_DC
+    CH_RANGE = 8 # PS400a_5V
+    ANALOG_OFFSET = 0
 
-# Set up channel H
-# handle = chandle
-# channel = PS4000a_CHANNEL_H = 1
-# enabled = 1
-# coupling type = PS4000a_DC = 1
-# range = PS4000a_2V = 7
-# analogOffset = 0 V
-chHRange = 7
-status["setChH"] = ps.ps4000aSetChannel(chandle, 7, 0, 1, chHRange, 0)
-assert_pico_ok(status["setChH"])
+    # Set up all channels
+    # handle = chandle
+    # channel = ch.num
+    # enabled = ENABLED
+    # coupling type = COUPLING_TYPE
+    # range = CH_RANGE
+    # analogOffset = ANALOG_OFFSET volts
+    for ch in channels:
+        status["setCh" + ch] = ps.ps400a
+        status["setCh" + ch] = ps.ps4000aSetChannel(chandle, ch.num, ENABLED, COUPLING_TYPE, CH_RANGE, ANALOG_OFFSET)
+        assert_pico_ok(status["setCh" + ch])
 
-# Set up single trigger
-# handle = chandle
-# enabled = 1
-# source = PS4000a_CHANNEL_A = 0
-# threshold = 1024 ADC counts
-# direction = PS4000a_RISING = 2
-# delay = 0 s
-# auto Trigger = 1000 ms
-status["trigger"] = ps.ps4000aSetSimpleTrigger(chandle, 1, 1, 8200, 2, 0,00) #This sets a trigger level of 1.25V. This WILL negelect -1.25 V. Needs optimization
-assert_pico_ok(status["trigger"])
+    threshold = input("Threshold: ")
+    DIRECTION = 2 # PS4000a_RISING
+    DELAY = 0
+    AUTO_TRIGGER = 0
+    # Set up single trigger
+    # handle = chandle
+    # enabled = 1
+    # source = channels[0].num
+    # threshold = user input
+    # direction = DIRECTION
+    # delay = 0 s
+    # auto Trigger = 0 ms
+    status["trigger"] = ps.ps4000aSetSimpleTrigger(chandle, ENABLED, channels[0].num, threshold, DIRECTION, DELAY, AUTO_TRIGGER)
+    assert_pico_ok(status["trigger"])
 
-# Set number of pre and post trigger samples to be collected
-preTriggerSamples = 20000
-postTriggerSamples = 60000
-maxSamples = preTriggerSamples + postTriggerSamples
+    # Set number of pre and post trigger samples to be collected
+    PRE_TRIGGER_SAMPLES = 20000
+    POST_TRIGGER_SAMPLES = 60000
+    MAX_SAMPLES = PRE_TRIGGER_SAMPLES + POST_TRIGGER_SAMPLES
 
-# Get timebase information
-# handle = chandle
-# timebase = 8 = timebase
-# noSamples = maxSamples
-# pointer to timeIntervalNanoseconds = ctypes.byref(timeIntervalns)
-# pointer to maxSamples = ctypes.byref(returnedMaxSamples)
-# segment index = 0
-timebase = 64   #Sample period of 1.24 uS
-timeIntervalns = ctypes.c_float()
-returnedMaxSamples = ctypes.c_int32()
-oversample = ctypes.c_int16(1)
-status["getTimebase2"] = ps.ps4000aGetTimebase2(chandle, timebase, maxSamples, ctypes.byref(timeIntervalns), ctypes.byref(returnedMaxSamples), 0)
-assert_pico_ok(status["getTimebase2"])
+    # Get timebase information
+    # handle = chandle
+    # timebase = 8 = timebase
+    # noSamples = MAX_SAMPLES
+    # pointer to timeIntervalNanoseconds = ctypes.byref(timeIntervalns)
+    # pointer to MAX_SAMPLES = ctypes.byref(returnedMaxSamples)
+    # segment index = 0
+    timebase = 64   #Sample period of 1.24 uS
+    timeIntervalns = ctypes.c_float()
+    returnedMaxSamples = ctypes.c_int32()
+    oversample = ctypes.c_int16(1)
+    status["getTimebase2"] = ps.ps4000aGetTimebase2(chandle, timebase, MAX_SAMPLES, ctypes.byref(timeIntervalns), ctypes.byref(returnedMaxSamples), 0)
+    assert_pico_ok(status["getTimebase2"])
 
-# Run block capture
-# handle = chandle
-# number of pre-trigger samples = preTriggerSamples
-# number of post-trigger samples = PostTriggerSamples
-# timebase = 3 = 80 ns = timebase (see Programmer's guide for mre information on timebases)
-# time indisposed ms = None (not needed in the example)
-# segment index = 0
-# lpReady = None (using ps4000aIsReady rather than ps4000aBlockReady)
-# pParameter = None
-status["runBlock"] = ps.ps4000aRunBlock(chandle, preTriggerSamples, postTriggerSamples, timebase, None, 0, None, None)
-assert_pico_ok(status["runBlock"])
+    # Run block capture
+    # handle = chandle
+    # number of pre-trigger samples = PRE_TRIGGER_SAMPLES
+    # number of post-trigger samples = PostTriggerSamples
+    # timebase = 3 = 80 ns = timebase (see Programmer's guide for mre information on timebases)
+    # time indisposed ms = None (not needed in the example)
+    # segment index = 0
+    # lpReady = None (using ps4000aIsReady rather than ps4000aBlockReady)
+    # pParameter = None
+    status["runBlock"] = ps.ps4000aRunBlock(chandle, PRE_TRIGGER_SAMPLES, POST_TRIGGER_SAMPLES, timebase, None, 0, None, None)
+    assert_pico_ok(status["runBlock"])
 
-# Check for data collection to finish using ps4000aIsReady
-ready = ctypes.c_int16(0)
-check = ctypes.c_int16(0)
-while ready.value == check.value:
-    status["isReady"] = ps.ps4000aIsReady(chandle, ctypes.byref(ready))
+    # Check for data collection to finish using ps4000aIsReady
+    ready = ctypes.c_int16(0)
+    check = ctypes.c_int16(0)
+    while ready.value == check.value:
+        status["isReady"] = ps.ps4000aIsReady(chandle, ctypes.byref(ready))
 
-# Create buffers ready for assigning pointers for data collection
-bufferAMax = (ctypes.c_int16 * maxSamples)()
-bufferAMin = (ctypes.c_int16 * maxSamples)() # used for downsampling which isn't in the scope of this example
-bufferBMax = (ctypes.c_int16 * maxSamples)()
-bufferBMin = (ctypes.c_int16 * maxSamples)() # used for downsampling which isn't in the scope of this example
-bufferCMax = (ctypes.c_int16 * maxSamples)()
-bufferCMin = (ctypes.c_int16 * maxSamples)() # used for downsampling which isn't in the scope of this example
-bufferDMax = (ctypes.c_int16 * maxSamples)()
-bufferDMin = (ctypes.c_int16 * maxSamples)() # used for downsampling which isn't in the scope of this example
+    SEGMENT_INDEX = 0
+    MODE = 0 # PS4000A_RATIO_MODE_NONE
 
-# Set data buffer location for data collection from channel A
-# handle = chandle
-# source = PS4000a_CHANNEL_A = 0
-# pointer to buffer max = ctypes.byref(bufferAMax)
-# pointer to buffer min = ctypes.byref(bufferAMin)
-# buffer length = maxSamples
-# segementIndex = 0
-# mode = PS4000A_RATIO_MODE_NONE = 0
-status["setDataBuffersA"] = ps.ps4000aSetDataBuffers(chandle, 0, ctypes.byref(bufferAMax), ctypes.byref(bufferAMin), maxSamples, 0 , 0)
-assert_pico_ok(status["setDataBuffersA"])
+    # Set data buffer location for data collection from channel A
+    # handle = chandle
+    # source = ch.num
+    # pointer to buffer max = ctypes.byref(buffer_max)
+    # pointer to buffer min = ctypes.byref(buffer_min)
+    # buffer length = MAX_SAMPLES
+    # segementIndex = SEGMENT_INDEX
+    # mode = MODE
+    for ch in channels:
+        buffer_max = (ctypes.c_int16 * MAX_SAMPLES)()
+        ch.b_max = buffer_max
+        buffer_min = (ctypes.c_int16 * MAX_SAMPLES)()
+        status["setDataBuffers" + ch.let] = ps.ps4000aSetDataBuffers(chandle, ch.num, ctypes.byref(buffer_max), ctypes.byref(buffer_min), MAX_SAMPLES, SEGMENT_INDEX, MODE)
+        assert_pico_ok(status["setDataBuffers" + ch.let])
 
-# Set data buffer location for data collection from channel B
-# handle = chandle
-# source = PS4000a_CHANNEL_B = 1
-# pointer to buffer max = ctypes.byref(bufferBMax)
-# pointer to buffer min = ctypes.byref(bufferBMin)
-# buffer length = maxSamples
-# segementIndex = 0
-# mode = PS4000A_RATIO_MODE_NONE = 0
-status["setDataBuffersB"] = ps.ps4000aSetDataBuffers(chandle, 1, ctypes.byref(bufferBMax), ctypes.byref(bufferBMin), maxSamples, 0 , 0)
-assert_pico_ok(status["setDataBuffersB"])
+    # create overflow location
+    overflow = ctypes.c_int16()
+    # create converted type MAX_SAMPLES
+    c_max_samples = ctypes.c_int32(MAX_SAMPLES)
 
-status["setDataBuffersC"] = ps.ps4000aSetDataBuffers(chandle, 2, ctypes.byref(bufferCMax), ctypes.byref(bufferCMin), maxSamples, 0 , 0)
-assert_pico_ok(status["setDataBuffersC"])
+    # Retried data from scope to buffers assigned above
+    # handle = chandle
+    # start index = 0
+    # pointer to number of samples = ctypes.byref(c_max_samples)
+    # downsample ratio = 0
+    # downsample ratio mode = PS4000a_RATIO_MODE_NONE
+    # pointer to overflow = ctypes.byref(overflow))
+    status["getValues"] = ps.ps4000aGetValues(chandle, 0, ctypes.byref(c_max_samples), 0, 0, 0, ctypes.byref(overflow))
+    assert_pico_ok(status["getValues"])
 
-status["setDataBuffersD"] = ps.ps4000aSetDataBuffers(chandle, 3, ctypes.byref(bufferDMax), ctypes.byref(bufferDMin), maxSamples, 0 , 0)
-assert_pico_ok(status["setDataBuffersD"])
+    # find maximum ADC count value
+    # handle = chandle
+    # pointer to value = ctypes.byref(maxADC)
+    maxADC = ctypes.c_int16(32767)
 
-# create overflow loaction
-overflow = ctypes.c_int16()
-# create converted type maxSamples
-cmaxSamples = ctypes.c_int32(maxSamples)
+    # convert ADC counts data to mV
+    for ch in channels:
+        ch.adc2VChMax = adc2mV(ch.b_max, CH_RANGE, maxADC)
 
-# Retried data from scope to buffers assigned above
-# handle = chandle
-# start index = 0
-# pointer to number of samples = ctypes.byref(cmaxSamples)
-# downsample ratio = 0
-# downsample ratio mode = PS4000a_RATIO_MODE_NONE
-# pointer to overflow = ctypes.byref(overflow))
-status["getValues"] = ps.ps4000aGetValues(chandle, 0, ctypes.byref(cmaxSamples), 0, 0, 0, ctypes.byref(overflow))
-assert_pico_ok(status["getValues"])
+    # Create time data
+    time = np.linspace(0, (c_max_samples.value) * timeIntervalns.value, c_max_samples.value)
 
+    # Plot data
+    fig, axs = plt.subplots(len(channels))
 
-# find maximum ADC count value
-# handle = chandle
-# pointer to value = ctypes.byref(maxADC)
-maxADC = ctypes.c_int16(32767)
+    i = 0
+    for ch in channels:
+        axs[i].plot(time, ch.adc2mVChMax[:])
+        axs[i].title.set_text("Channel " + ch.let)
+        i = i + 1
 
-# convert ADC counts data to mV
-adc2mVChAMax =  adc2mV(bufferAMax, chARange, maxADC)
-adc2mVChBMax =  adc2mV(bufferBMax, chBRange, maxADC)
-adc2mVChCMax =  adc2mV(bufferCMax, chCRange, maxADC)
-adc2mVChDMax =  adc2mV(bufferDMax, chDRange, maxADC)
-# Create time data
-time = np.linspace(0, (cmaxSamples.value) * timeIntervalns.value, cmaxSamples.value)
+    plt.xlabel('Time (ns)')
+    plt.ylabel('Voltage (mV)')
+    plt.show()
 
-# plot data from channel A and B
-fig, axs = plt.subplots(4)
-axs[0].plot(time, adc2mVChAMax[:],"r-")
-axs[1].plot(time, adc2mVChBMax[:],"b-")
-axs[2].plot(time, adc2mVChCMax[:],"o-")
-axs[3].plot(time, adc2mVChDMax[:],"g-")
-axs[0].title.set_text("Channel A")
-axs[1].title.set_text("Channel B")
-axs[2].title.set_text("Channel C")
-axs[3].title.set_text("Channel D")
-plt.xlabel('Time (ns)')
-plt.ylabel('Voltage (mV)')
-plt.show()
+    file_name = input("Input File Name you slimy cunt:")
 
-file_name = input("Input File Name you slimy cunt:")
+    new_file = open(file_name,'w')
+    new_file.close()
+    titles = ["time"]
+    for ch in channels:
+        titles.append("Channel" + ch.let)
 
-new_file = open(file_name,'w')
-new_file.close()
-titles = ["time","ChannelA","ChannelB","ChannelC","ChannelD"]
-with open(file_name,'a') as f:
-    writer = csv.writer(f)
-    writer.writerow(titles)
-    writer.writerow(time)
-    writer.writerow(adc2mVChAMax[:])
-    writer.writerow(adc2mVChBMax[:])
-    writer.writerow(adc2mVChCMax[:])
-    writer.writerow(adc2mVChDMax[:])
+    with open(file_name,'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(titles)
+        writer.writerow(time)
+        for ch in channels:
+            writer.writerow(ch.adc2mVMax[:])
 
+    # Stop the scope
+    # handle = chandle
+    status["stop"] = ps.ps4000aStop(chandle)
+    assert_pico_ok(status["stop"])
 
+    # Close unitDisconnect the scope
+    # handle = chandle
+    status["close"] = ps.ps4000aCloseUnit(chandle)
+    assert_pico_ok(status["close"])
 
-
-
-# Stop the scope
-# handle = chandle
-status["stop"] = ps.ps4000aStop(chandle)
-assert_pico_ok(status["stop"])
-
-# Close unitDisconnect the scope
-# handle = chandle
-status["close"] = ps.ps4000aCloseUnit(chandle)
-assert_pico_ok(status["close"])
-
-# display status returns
-print(status)
-
+    # display status returns
+    print(status)
