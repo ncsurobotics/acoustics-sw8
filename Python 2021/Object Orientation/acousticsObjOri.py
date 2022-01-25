138aca35f528cfa633d899295603ef18cb54c4c9
Bennett Petzold <bennettpetzold01@gmail.com>
2022-01-24 19:48:18 -0500

    Remove excess spaces

--- Python 2021/Object Orientation/acousticsObjOri.py
+++ Python 2021/Object Orientation/acousticsObjOri.py
@@ -1,7 +1,5 @@
-
 class Acoustics:
 
-    
     def __init__(self, run_name, num_channels, delta_x, delta_z):
         self.pitch = 0;
         self.yaw = 0;
@@ -10,14 +8,14 @@ class Acoustics:
         self.delta_x = delta_x
         self.delta_z = delta_z
         self.delta_t = 0;
-    
+
     def initialize(self):
         '''
             Find a way to write this in such a way that it is a loop instead of brute.
             Potentially allows code to be somewhat reusable under more hydrophones
         '''
         return 0
-        
+
     def data_sample(self):
         '''
             Find a way to write this in such a way that it is a loop instead of brute.
@@ -29,13 +27,13 @@ class Acoustics:
         '''
             Convolutional function to determine the delta t value based on cross correlation
         '''
-        
+
         cross_correlation = [0]
-        
-        
+
+
         k = length(channel_one)
         i = length(channel_two)
-        
+
         for n in range(0, k + i - 1):
             cross_correlation[n] = 0
             for m in range(0,k - 1):
@@ -44,18 +42,16 @@ class Acoustics:
                 else:
                     cross_correlation[n] = cross_correlation[n] + (channel_two[m] * channel_one[n + m - (k - 1)]
             cross_correlation.append(0)
-        
+
         return cross_correlation.index(max(cross_correlation))
-        
+
     def c_val(delta_a, delta_t):
-        d = delta_t * 1480  
-        c = (d**2) / ( (delta_a**2) - d**2) 
-    
+        d = delta_t * 1480
+        c = (d**2) / ( (delta_a**2) - d**2)
+
     def pitch_yaw(self,C1,C2):
         arg1 = math.sqrt( (C1 + 1) / (C2 + C1 * C2) )
         arg2 = math.sqrt( (C1 + C1 * C2) / (1 - C1 * C2) )
-        
+
         self.pitch = math.atan(arg1)
         self.yaw = math.atan(arg2)
-        
-        
